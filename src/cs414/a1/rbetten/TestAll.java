6b27d45f381e086e419d99cab554a177144da22a
rebettendorf <ren.bettendorf@karlstorz.com>
2016-09-08 16:45:18 -0700

    Added two ways to run from command line

--- src/cs414/a1/rbetten/TestAll.java
+++ src/cs414/a1/rbetten/TestAll.java
@@ -10,5 +10,18 @@ import junit.textui.TestRunner;
 @RunWith(Suite.class)
 @SuiteClasses({ CompanyTest.class, ProjectTest.class, QualificationTest.class, WorkerTest.class })
 public class TestAll {
+	public static void main(String[] args) {
+	    Result result = JUnitCore.runClasses(CompanyTest.class, ProjectTest.class, QualificationTest.class, WorkerTest.class);
+		
+    }
+	/*
+	public static void main(String[] args)
+	{
+		junit.textui.TestRunner.run(suite());
+	}
 	
+	public static junit.framework.Test suite()
+	{
+			return new JUnit4TestAdapter(AllTests.class);
+	}
 }
