2283e27b809c7e36dbae08ca93148703324d7fd9
Ren <Renaldorini@gmail.com>
2016-09-08 21:05:20 -0700

    Working product on madison.cs.colostate.edu with command java -cp /usr/share/java/junit.jav:/usr/share/java/hamcrest/core.jar:. cs414.a1.rbetten/TestAll

--- src/cs414/a1/rbetten/TestAll.java
+++ src/cs414/a1/rbetten/TestAll.java
@@ -1,16 +1,14 @@
 package cs414.a1.rbetten;
 
-import org.junit.runner.JUnitCore;
-import org.junit.runner.Result;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
 
+import junit.framework.JUnit4TestAdapter;
 
 @RunWith(Suite.class)
-@SuiteClasses({ QualificationTest.class })
+@Suite.SuiteClasses({ CompanyTest.class, ProjectTest.class, QualificationTest.class, WorkerTest.class })
 public class TestAll {
 	public static void main(String[] args) {
-	    Result result = JUnitCore.runClasses(QualificationTest.class);
-	  }
+		junit.textui.TestRunner.run(new JUnit4TestAdapter(TestAll.class));
+	}
 }
